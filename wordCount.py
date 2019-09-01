78c99cc96cfac4bb27b8a84d56eed364ace1cca2
Cristian Molina <cmutep@gmail.com>
2019-09-01 11:04:06 -0600

    passes test -- needs to get bash commands

--- wordCount.py
+++ wordCount.py
@@ -3,10 +3,11 @@ import string
 import operator
 
 frequency = {}
-document_text = open('declaration.txt', 'r')
+document_text = open('speech.txt', 'r')
 text_string = document_text.read()
-match_pattern = re.findall(r'\b[a-zA-Z]{0,15}\b', text_string)
-
+match_pattern = re.findall(r'\b[a-zA-Z]{1,30}\b', text_string)
+print(len(match_pattern))
+match_pattern = [x.lower() for x in match_pattern]
 # populating dictionary
 for word in match_pattern:
     if word == '':
@@ -15,23 +16,22 @@ for word in match_pattern:
     frequency[word] = count + 1
 
 # sorting the dictionary
-sorted_frequency = sorted(
-    frequency.items(), key=operator.itemgetter(1), reverse=True)
+sorted_frequency = sorted(frequency.items())
 # print(sorted_frequency)
 
 
 frequency_list = frequency.keys()
 # print(frequency_list)
 
-print(range(len(sorted_frequency)))
+# print(range(len(sorted_frequency)))
 
 f = open("myOutput.txt", "w+")
 for i in range(len(sorted_frequency)):
     index = i
-    print("word: " + str(i))
-    tuple = sorted_frequency[i]
-    print("first: " + str(tuple[0]))
-    print("second: " + str(tuple[1]))
 
-    #print(str(index) + " " + str(tuple))
+    tuple = sorted_frequency[i]
+    #print("first: " + str(tuple[0]))
+    #print("second: " + str(tuple[1]))
+
+    print(str(str(tuple[0])) + " " + str(tuple[1]))
     f.write(str(str(tuple[0])) + " " + str(tuple[1]) + "\n")
