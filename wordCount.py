80d792d199977a2b73e56f6b305c12c7269d156a
Cristian Molina <cmutep@gmail.com>
2019-09-01 09:57:07 -0600

    only needs to take input from terminal

--- wordCount.py
+++ wordCount.py
@@ -1,6 +1,5 @@
 import re
 import string
-import functools
 import operator
 
 frequency = {}
@@ -8,17 +7,31 @@ document_text = open('declaration.txt', 'r')
 text_string = document_text.read()
 match_pattern = re.findall(r'\b[a-zA-Z]{0,15}\b', text_string)
 
+# populating dictionary
 for word in match_pattern:
     if word == '':
         continue
     count = frequency.get(word, 0)
     frequency[word] = count + 1
 
+# sorting the dictionary
+sorted_frequency = sorted(
+    frequency.items(), key=operator.itemgetter(1), reverse=True)
+# print(sorted_frequency)
+
+
 frequency_list = frequency.keys()
+# print(frequency_list)
+
+print(range(len(sorted_frequency)))
 
 f = open("myOutput.txt", "w+")
-for words in frequency_list:
-    w = words
-    num = frequency[words]
-    print("writing...")
-    f.write(str(w) + " " + str(num) + "\n")
+for i in range(len(sorted_frequency)):
+    index = i
+    print("word: " + str(i))
+    tuple = sorted_frequency[i]
+    print("first: " + str(tuple[0]))
+    print("second: " + str(tuple[1]))
+
+    #print(str(index) + " " + str(tuple))
+    f.write(str(str(tuple[0])) + " " + str(tuple[1]) + "\n")
