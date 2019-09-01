8f95d868d7446c16bbcbefd8d5a604313c30ab47
Cristian Molina <cmutep@gmail.com>
2019-08-31 21:13:21 -0600

    now its case sensitive and ignores spaces

--- wordCount.py
+++ wordCount.py
@@ -1,16 +1,22 @@
 import re
 import string
+import functools
+import operator
+
 frequency = {}
 document_text = open('declaration.txt', 'r')
-text_string = document_text.read().lower()
-match_pattern = re.findall(r'\b[a-z]{0,15}\b', text_string)
+text_string = document_text.read()
+match_pattern = re.findall(r'\b[a-zA-Z]{0,15}\b', text_string)
 
 for word in match_pattern:
+    if word == '':
+        continue
     count = frequency.get(word, 0)
     frequency[word] = count + 1
 
 frequency_list = frequency.keys()
 
+f = open("myOutput.txt", "w+")
 for words in frequency_list:
     toPrint = words, frequency[words]
     print(toPrint)
